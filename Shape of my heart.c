3619a5d35b88302484d9459dd1b1f36dbfdb742c
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-06-25 00:00:42 +0900

    반복문 연습

---
+++ Shape of my heart.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int s1, s2;
+	printf("사각형입니다.\n\n");
+	
+	for(s1 = 0;s1 < 10;s1++)
+	{
+		for(s2 = 0;s2 < 10;s2++)
+		{
+			printf("# ");
+		}
+		printf("\n");
+	}
+	
+	printf("\n\n피라미드입니다.\n");
+	
+	int p1, p2;
+	
+	for(p1 = 0;p1 < 10;p1++)
+	{
+		for(p2 = 10 - p1;p2 > 0;p2--)
+		{
+			printf("  ");
+		}
+		
+		for(p2 = p1 - 1;p2 > 0;p2--)
+		{
+			printf("^ ");
+		}
+		
+		for(p2 = p1 - 2;p2 > 0;p2--)
+		{
+			printf("^ ");
+		}
+		
+		printf("\n");
+	}
+	
+	return 0;
+}
