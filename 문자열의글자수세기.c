0af3acee1de73dd8ccd5eaf226888e45925b1880
ArinJBJ <qwert1424@naver.com>
2020-07-02 23:55:26 +0900

    문자열 연습

--- 문자열의글자수세기.c
+++ 문자열의글자수세기.c
@@ -3,12 +3,13 @@
 int main()
 {
 	char input[1001];
-	fgets(input, sizeof(input - 1), stdin);
+	fgets(input, sizeof(input), stdin);
 	int count = 0;
 	while(input[count] != '\0')
 	{
 		count++;
 	}
 	printf("입력한 문자열의 글자 수는 %d입니다.\n", count - 1);
+	printf("입력한 문자열 : %s", input);
 	return 0;
 }
